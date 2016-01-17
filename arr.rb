1548eb74f05d4650bc04f2cfd345578b14b93c6e
juan David <jdavehacker@gmail.com>
2016-01-17 10:15:08 -0500

    Ejercicio con array

---
+++ arr.rb
@@ -0,0 +1,14 @@
+arr = Array.new 4
+i = 0
+
+while i < arr.length
+	puts "Ingrese le elemento #{i}"
+	arr[i] = gets.chomp
+	i += 1
+end
+
+i = 0
+while i < arr.length
+	puts "#{i} : #{arr[i]}"
+	i += 1
+end
